5f70d732af448945f6637364b55db44425a86df1
Cybernetics <79071277+shade234sherif@users.noreply.github.com>
2022-03-29 00:41:27 +0100

    Add files via upload

---
+++ netcode.py
@@ -0,0 +1,20 @@
+import os 
+import sys 
+from mechanize import request
+if sys.version_info[0] !=3:
+    print('please do install python3')
+
+from ast import Try, Break 
+
+print('(⓿_⓿) i can help you get a web page source code (‾◡◝)')
+
+Break
+url =input('paste web page link here:')
+
+print('\033[2;43m (⌐■_■) compiling codes for better view \033[2;43m')
+
+Break
+
+x = url 
+request.get(url)
+print(x.content)
---
+++ netphy.py
@@ -0,0 +1,80 @@
+import sys
+import os 
+
+from ast import Try, excepthandler , Break
+
+if sys.version_info[0] !=3:
+    print('n\.......please install python3 to run tool.....\n')
+Try;
+from mechanize import request
+
+excepthandler
+print("n\....please do install mechanize to run program\n")
+sys.exit
+
+print("\033[2;31m.-..-..---..---..---..-. .-..-..-.\033[0;0m")
+print("\033[2;31m| .` || |- `| |'| |-'| |=| | >  /\033[0;0m") 
+print("\033[2;31m`-'`-'`---' `-' `-'  `-' `-' `-'\033[0;0m") 
+
+Break;
+
+url = input("paste web https link here:")
+print('\n(^_-)db(-_^)please wait while i compile the data received\n')
+
+Break;
+
+print('-------status code-------')
+x = url
+request.get(url)
+print(x.statuscode)
+
+print('-------status [ok]-------')
+x = url
+request.get(url)
+print(x.ok)
+
+print('------status condition---')
+x = url
+request.get(url)
+print(x.reason)
+
+print('----------links----------')
+x = url 
+request.get(url)
+print(x.links)
+
+print('-is this a redirect link-')
+x = url 
+request.get(url)
+print(x.is_redirect)
+
+print('-------perma link--------')
+x = url
+request.get(url)
+print(x.is_permanent_redirect)
+
+print('-------cookies-----------')
+x = url
+request.get(url)
+print(x.cookies)
+
+print('------recorded logs------')
+x = url 
+request.get(url)
+print(x.json())
+
+print('-------encode------------')
+x = url 
+request.get(url)
+print(x.encoding)
+
+print('-------IP address--------')
+x = url
+request.get(url)
+print(x.ipaddress)
+
+print('-------History-----------')
+x = url 
+request.get(url)
+print(x.history)
+
