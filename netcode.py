5371921a16ae815391fa1dea625c115c70632a9f
Cybernetics <79071277+shade234sherif@users.noreply.github.com>
2022-04-01 08:51:42 +0100

    Add files via upload

--- netcode.py
+++ netcode.py
@@ -1,23 +1,21 @@
 import os 
-import mechanize 
+import tokenize 
 import sys 
 
-Try
-from mechanize import request
 if sys.version_info[0] !=3:
     print('please do install python3')
 
 from ast import Try, Break 
-
+from urllib import request
 print('(⓿_⓿) i can help you get a web page source code (‾◡◝)')
 
 Break
 url =input('paste web page link here:')
 
-print('\033[2;43m (⌐■_■) compiling codes for better view \033[2;43m')
+print('\033[2;43m (⌐■_■) compiling codes for better view \033[0;0m')
 
 Break
 
 x = url 
-request.get(url)
+request.post(url)
 print(x.content)
--- netphy.py
+++ netphy.py
@@ -1,38 +1,54 @@
+from email.header import Header
+from http.client import REQUEST_TIMEOUT
+from ssl import VERIFY_ALLOW_PROXY_CERTS
 import sys
-import mechanize 
+import urllib
 import os 
 
+
 from ast import Try, excepthandler , Break
+from webbrowser import get
 
 if sys.version_info[0] !=3:
     print('n\.......please install python3 to run tool.....\n')
 Try;
-from mechanize import request
+import request
 
 excepthandler
-print("n\....please do install mechanize to run program\n")
 sys.exit
+print("|----------------------------------------|")
+print("|----------------------------------------|")
+print('|\033[2;32m A TOOL BY CYBERHACKS\033[0;0m-------------------|')
+print('|----------------------------------------|')
+print("|\033[2;32m DO FOLLOW ON FB @CYBERHACKS6\033[0;0m-----------|")
+print('|----------------------------------------|')
+print("|----------------------------------------|")
+
+question = input('enter any key to continue:')
+
+Break;
 
 print("\033[2;31m.-..-..---..---..---..-. .-..-..-.\033[0;0m")
 print("\033[2;31m| .` || |- `| |'| |-'| |=| | >  /\033[0;0m") 
 print("\033[2;31m`-'`-'`---' `-' `-'  `-' `-' `-'\033[0;0m") 
 
-Break;
 
 url = input("paste web https link here:")
 print('\n(^_-)db(-_^)please wait while i compile the data received\n')
 
-Break;
+Break 
 
 print('-------status code-------')
-x = url
-request.get(url)
-print(x.statuscode)
+x = request.get(url)
+print(x.status_code)
+excepthandler
+REQUEST_TIMEOUT
+print("failed to connect")
 
 print('-------status [ok]-------')
 x = url
 request.get(url)
-print(x.ok)
+print(x.header['ok'])
 
 print('------status condition---')
 x = url
@@ -45,7 +61,7 @@ request.get(url)
 print(x.links)
 
 print('-is this a redirect link-')
-x = url 
+x = url
 request.get(url)
 print(x.is_redirect)
 
@@ -57,7 +73,7 @@ print(x.is_permanent_redirect)
 print('-------cookies-----------')
 x = url
 request.get(url)
-print(x.cookies)
+print(x.header['cookies'])
 
 print('------recorded logs------')
 x = url 
@@ -65,7 +81,7 @@ request.get(url)
 print(x.json())
 
 print('-------encode------------')
-x = url 
+x = url
 request.get(url)
 print(x.encoding)
 
@@ -75,7 +91,11 @@ request.get(url)
 print(x.ipaddress)
 
 print('-------History-----------')
-x = url 
+x = url
 request.get(url)
-print(x.history)
+print(x.header['server'])
 
+print('---------via-------------')
+x = url
+request.get(url)
+print(x.header[VERIFY_ALLOW_PROXY_CERTS])
